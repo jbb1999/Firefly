dec9c4732ea1c87b7286e918124651eaa6f26168
noturperson <jens.bruvik@gmail.com>
2022-07-01 10:40:30 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+invoke_char = "!" #Used to summon the bot, example !ban
+discord_activity = 3 #changes the status of bot, example 0 is nothing, 1 is playing, 2 is listening, and 3 is watching
+discord_activity_name = "in progress"
+
+
+token=""
